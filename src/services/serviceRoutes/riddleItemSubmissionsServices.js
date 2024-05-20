d012197264f4bb6a311c979495aac99eb2265514
aspenos <arisspenos1@gmail.com>
2024-05-20 15:56:53 -0400

    create riddle item submission function

--- src/services/serviceRoutes/riddleItemSubmissionsServices.js
+++ src/services/serviceRoutes/riddleItemSubmissionsServices.js
@@ -2,6 +2,11 @@ import { api } from "../apiConnection";
 
 // TODO: createRiddleItemSubmission (payload)
 
+// CREATE NEW RIDDLE ITEM SUBMISSION
+
+export const getRiddleItemSubmissions = async (huntTemplateId, riddleItemId, participationId) => {
+  return await api.get(`/hunt-templates/${huntTemplateId}/riddle-items/${riddleItemId}/participations/${participationId}/riddle-item-submissions/`);
+};
 
 // TODO: updateRiddleItemSubmissionById (payload)
 
