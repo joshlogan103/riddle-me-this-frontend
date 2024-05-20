6ac6f95beb402f45ef173b526a5bcaad376a3dc6
aspenos <arisspenos1@gmail.com>
2024-05-20 16:10:48 -0400

    get new riddle item submission function

--- src/services/serviceRoutes/riddleItemSubmissionsServices.js
+++ src/services/serviceRoutes/riddleItemSubmissionsServices.js
@@ -4,11 +4,30 @@ import { api } from "../apiConnection";
 
 // CREATE NEW RIDDLE ITEM SUBMISSION
 
-export const getRiddleItemSubmissions = async (huntTemplateId, riddleItemId, participationId) => {
-  return await api.get(`/hunt-templates/${huntTemplateId}/riddle-items/${riddleItemId}/participations/${participationId}/riddle-item-submissions/`);
+export const createRiddleItemSubmission = async (huntTemplateId, riddleItemId, participationId, payload) => {
+  return await api.post(`/hunt-templates/${huntTemplateId}/riddle-items/${riddleItemId}/participations/${participationId}/riddle-item-submissions/`, payload, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 // TODO: updateRiddleItemSubmissionById (payload)
 
+// UPDATE A RIDDLE ITEM SUBMISSION BY ID
+
+export const updateRiddleItemSubmissionByID = async (huntTemplateId, riddleItemId, participationId, riddleItemSubmissionId, payload) => {
+  return await api.put(`/hunt-templates/${huntTemplateId}/riddle-items/${riddleItemId}/participations/${participationId}/riddle-item-submissions/${riddleItemSubmissionId}/`, payload, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 // TODO: do we need GET or DELETE ??
+
+// GET NEW RIDDLE ITEM SUBMISSION
+
+export const getRiddleItemSubmissions = async (huntTemplateId, riddleItemId, participationId) => {
+  return await api.get(`/hunt-templates/${huntTemplateId}/riddle-items/${riddleItemId}/participations/${participationId}/riddle-item-submissions/`);
+};
