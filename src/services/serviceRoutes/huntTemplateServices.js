5365f67c7a790f7bc42b4e3e402b58f8df30e8f7
aspenos <arisspenos1@gmail.com>
2024-05-20 15:22:46 -0400

    Create new hunt template

--- src/services/serviceRoutes/huntTemplateServices.js
+++ src/services/serviceRoutes/huntTemplateServices.js
@@ -6,7 +6,7 @@ export const getHuntTemplatesByUser = async (userId) => {
   return await api.get(`/users/${userId}/hunt-templates/`);
 }
 
-// CREATE NEW HUNT TEMPLATE
+// CREATE NEW HUNT TEMPLATTE
 
 export const createHuntTemplate = async (payload) => {
   return await api.post(`/hunt-templates/`, payload, {
