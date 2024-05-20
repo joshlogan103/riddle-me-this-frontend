5b795e2d7e9fa0c851cc806f318ec783c9bf0385
aspenos <arisspenos1@gmail.com>
2024-05-20 15:16:09 -0400

    Implement createHuntInstance and getHuntInstanceByTemplate

---
+++ src/services/serviceRoutes/huntInstanceServices.js
@@ -0,0 +1,18 @@
+import { api } from "../apiConnection";
+
+// HUNT INSTANCE
+
+export const createHuntInstance = async (huntTemplateId, payload) => {
+  return await api.post(`/hunt-templates/${huntTemplateId}/hunt-instances/`, payload, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+// GET HUNT INSTANCE
+
+export const getHuntInstancesByTemplate = async (huntTemplateId) => {
+  return await api.get(`/hunt-templates/${huntTemplateId}/hunt-instances/`);
+};
+
