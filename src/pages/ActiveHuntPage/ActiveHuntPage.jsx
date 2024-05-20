c0522f3d1cf80848ccfd2a3e9ef74d03d0533358
aspenos <arisspenos1@gmail.com>
2024-05-20 11:17:26 -0400

    Delete pages folder

--- src/pages/ActiveHuntPage/ActiveHuntPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ActiveHuntPage = () => {
-  return (
-    <div>ActiveHuntPage</div>
-  )
-}
-
-export default ActiveHuntPage
--- src/pages/BrowseHunts/BrowseHunts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BrowseHunts = () => {
-  return (
-    <div>BrowseHunts</div>
-  )
-}
-
-export default BrowseHunts
