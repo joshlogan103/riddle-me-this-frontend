8f394b2e6faf15566bc4ab419ae9de2108089721
dfergusbrown <dfergusbrown@gmail.com>
2024-05-21 12:23:11 -0400

    minimal styling to navbar and app

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,27 @@ import HuntDetails from "./pages/HuntDetails/HuntDetails.jsx"
 import LaunchHunt from "./pages/LaunchHunt/LaunchHunt.jsx"
 import MyProfile from "./pages/MyProfile/MyProfile.jsx"
 import ProtectedRoutes from "./protected_routes/ProtectedRoutes.jsx";
+import "./App.css"
 
 function App() {
 
   return (
-    <>
+    <div className="main-container">
       <Navbar />
+      <div className="body-container">
       <Routes>
         <Route path="/" element={<LandingPage />}/>
         <Route path="/browse" element={<BrowseHunts />} />
         <Route path="/active-hunt" element={<ProtectedRoutes><ActiveHuntPage /></ProtectedRoutes>} />
         <Route path="/create-hunt" element={<CreateHuntTemplate />} />
         <Route path="/creator-control-panel" element={<CreatorControlPanel />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/hunt-details" element={<HuntDetails />} />
         <Route path="/launch-hunt" element={<LaunchHunt />} />
         <Route path="/profile" element={<MyProfile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
-    </>
+      </div>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Theme>
+    <Theme accentColor="indigo" appearance="dark">
       <BrowserRouter>
         <App />
       </BrowserRouter>
