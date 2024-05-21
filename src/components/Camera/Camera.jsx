693628a910ccf89188ac9cae62a5cb14f5e5a962
vitrineofcode <tilers_unable_0n@icloud.com>
2024-05-21 15:53:39 -0400

    updates to code in Camera..jsx. . removal of un-needed cookies and csrf code

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -2,22 +2,6 @@ import { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import './Camera.css';
 
-const getCookie = (name) => {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-};
-
 const Camera = () => {
   const webcamRef = useRef(null);
   const [cameraOpen, setCameraOpen] = useState(false);
@@ -33,14 +17,12 @@ const Camera = () => {
     setCameraOpen(false);
 
     try {
-      const csrfToken = getCookie('csrftoken');
       const response = await fetch('http://127.0.0.1:8000/api/upload/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'X-CSRFToken': csrfToken,
         },
-        body: JSON.stringify({ image: imageSrc }),
+        body: JSON.stringify({ image: imageSrc, label: "shower_curtain"}),
       });
 
       if (!response.ok) {
