8e9171ebbfbee4ee52dcfffbc166c0f60a1d64e8
vitrineofcode <tilers_unable_0n@icloud.com>
2024-05-21 17:47:14 -0400

    switch route from temp api/upload to riddle-item-submissions

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -17,7 +17,7 @@ const Camera = () => {
     setCameraOpen(false);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/upload/', {
+      const response = await fetch('http://127.0.0.1:8000/api/hunt-templates/1/riddle-items/1/participations/1/riddle-item-submissions/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
