b6260e915010a257fa9818573b228ab20107e426
dfergusbrown <dfergusbrown@gmail.com>
2024-05-21 17:41:36 -0400

    combines and toggles login and register dialogs

--- src/components/LoginDialog/LoginDialog.jsx
+++ src/components/LoginDialog/LoginDialog.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { Dialog, Flex, Button } from "@radix-ui/themes";
-import { TextField, Text } from "@radix-ui/themes";
+import React, { useState } from "react";
+import { Dialog, Flex, Button, TextField, Text } from "@radix-ui/themes";
 
-const LoginDialog = ({buttonName}) => {
+const LoginDialog = ({ buttonName }) => {
+  const [registering, setRegistering] = useState(false);
+
+  const toggleRegistering = () => {
+    setRegistering(!registering)
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -10,41 +15,54 @@ const LoginDialog = ({buttonName}) => {
       </Dialog.Trigger>
 
       <Dialog.Content maxWidth="450px">
-        <Dialog.Title>Edit profile</Dialog.Title>
+        <Dialog.Title>{registering ? "Create Account" : "Login"}</Dialog.Title>
         <Dialog.Description size="2" mb="4">
-          Make changes to your profile.
+          You must login before performing this function
         </Dialog.Description>
 
         <Flex direction="column" gap="3">
+          {registering ? (
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Email
+              </Text>
+              <TextField.Root
+                defaultValue="Bob the Builder"
+                placeholder="Enter username"
+              />
+            </label>
+          ) : null}
           <label>
             <Text as="div" size="2" mb="1" weight="bold">
-              Name
+              Username
             </Text>
             <TextField.Root
-              defaultValue="Freja Johnsen"
-              placeholder="Enter your full name"
+              defaultValue="Bob the Builder"
+              placeholder="Enter username"
             />
           </label>
           <label>
             <Text as="div" size="2" mb="1" weight="bold">
-              Email
+              Password
             </Text>
             <TextField.Root
-              defaultValue="freja@example.com"
-              placeholder="Enter your email"
+              defaultValue=""
+              placeholder="password"
+              type="password"
             />
           </label>
         </Flex>
         <Flex gap="3" mt="4" justify="end">
           <Dialog.Close>
-            <Button variant="soft" color="gray">
+            <Button variant="surface" color="gray">
               Cancel
             </Button>
           </Dialog.Close>
           <Dialog.Close>
-            <Button>Save</Button>
+            <Button variant="surface">Submit</Button>
           </Dialog.Close>
         </Flex>
+        <p>Need to <a onClick={toggleRegistering}>Create an Account?</a></p>
       </Dialog.Content>
     </Dialog.Root>
   );
