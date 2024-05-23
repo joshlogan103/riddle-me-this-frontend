25a15ddb7587cd4a00a5d0637b983cc69ea9ad74
Josh Logan <joshlogan103@gmail.com>
2024-05-23 11:14:18 -0400

    add functionality for riddle item submission to riddle layout and camera components

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -5,7 +5,8 @@ import './Camera.css';
 import { createRiddleItemSubmission } from '../../services/serviceRoutes/riddleItemSubmissionsServices';
 
 const Camera = (props) => {
-  const { riddles } = props;
+  const { riddle } = props;
+
   const webcamRef = useRef(null);
   const [cameraOpen, setCameraOpen] = useState(false);
   const [imageSrc, setImageSrc] = useState(null);
@@ -29,19 +30,25 @@ const Camera = (props) => {
   };
 
   const submitImage = async () => {
-    console.log("Riddles object:", riddles); 
+    console.log("Riddles object:", riddle); 
 
     try {
-      const response = await createRiddleItemSubmission(riddles.scavenger_hunt.id, riddles.id, '1', { image: imageSrc });
+      console.log(riddle)
 
-      if (!response.ok) {
+      // export const createRiddleItemSubmission = async (huntTemplateId, riddleItemId, participationId, payload)
+      console.log(riddle.scavenger_hunt.id, riddle.id, riddle.item.name)
+      const response = await createRiddleItemSubmission(riddle.scavenger_hunt.id, riddle.id, '1', { image: imageSrc, label: riddle.item.name });
+
+      if (!response.status == 200) {
         setCameraOpen(false);
+        setImageSrc(null);
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const data = await response.json();
+      const data = response.data
       setResponseMessage(data.correct ? "Object Present: True" : "Object Present: False");
       setCameraOpen(false);
+      setImageSrc(null);
     } catch (error) {
       console.error('Error sending image to predictions:', error);
       setResponseMessage('Error sending image to predictions');
