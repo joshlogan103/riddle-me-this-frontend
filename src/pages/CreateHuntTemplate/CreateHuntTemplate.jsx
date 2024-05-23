190f2b48b64aacb681741f47b4a9d1d36e544489
dfergusbrown <dfergusbrown@gmail.com>
2024-05-23 14:27:00 -0400

    conditionally renders createhunttemplate elements to first create hunt and then add riddle items.  Connects each to back end functionality using apiservices

--- src/pages/CreateHuntTemplate/CreateHuntTemplate.jsx
+++ src/pages/CreateHuntTemplate/CreateHuntTemplate.jsx
@@ -9,22 +9,33 @@ import {
   Table,
 } from "@radix-ui/themes";
 import { getAllItems } from "../../services/serviceRoutes/itemServices";
+import RiddleItemList from "./RiddleItemList";
+import RiddleItemSelection from "./RiddleItemSelection";
+import CreateHuntInputs from "./CreateHuntInputs";
+import SubmitHuntBtn from "./SubmitHuntBtn";
+import { createHuntTemplate } from "../../services/serviceRoutes/huntTemplateServices";
+import { createRiddleItemByTemplate } from "../../services/serviceRoutes/riddleItemServices";
+import { useNavigate } from "react-router";
 
 const CreateHuntTemplate = () => {
   const [selection, setSelection] = useState({
-    huntName: "",
-    description: "",
     category: "",
     item: "",
-    id: 0,
+    item_id: 0,
     riddle: "",
   });
-  const [huntName, setHuntName] = useState("")
-  const [description, setDescription] = useState("")
+  const [huntName, setHuntName] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
   const [categories, setCategories] = useState([]);
   const [savedItems, setSavedItems] = useState([]);
   const [items, setItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
+
+  const [isTempCreated, setIsTempCreated] = useState(false);
+  const [newHuntId, setNewHuntId] = useState(null)
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     // TODO: connect to fetch all items
@@ -74,7 +85,7 @@ const CreateHuntTemplate = () => {
   };
 
   const handleItemChange = (name, id) => {
-    const newSelection = { ...selection, item: name, id: id };
+    const newSelection = { ...selection, item: name, item_id: id };
     setSelection(newSelection);
   };
 
@@ -83,11 +94,37 @@ const CreateHuntTemplate = () => {
     setSelection(newSelection);
   };
 
-  const saveHunt = () => {
+  const createHunt = async () => {
     // TODO: Create Hunt API
-
-    console.log("Saving hunt", { huntName, description, categories });
+    try {
+      const response = await createHuntTemplate({name: huntName, description: description, location: location, difficulty: "E"})
+      console.log(response)
+      if (response.status === 201) {
+        setNewHuntId(response.data.id)
+        setIsTempCreated(true)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    // console.log("Creating hunt", { huntName, description, categories });
   };
+
+  const saveAndExit = async() => {
+    const updatedPayload = []
+    savedItems.forEach(item => {
+      updatedPayload.push({item_id: item.item_id, riddle: item.riddle })
+    })
+    try {
+      const response = await createRiddleItemByTemplate(newHuntId, updatedPayload)
+      console.log(response)
+      if (response.status === 201) {
+        navigate("/creator-control-panel")
+        console.log("riddle items sent and saved!")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <Flex
@@ -96,182 +133,42 @@ const CreateHuntTemplate = () => {
       gap="20px"
       style={{ padding: "20px" }}
     >
-      <Text as="h1" size="6" weight="bold" color="indigo" variant="soft">
-        Create a Scavenger Hunt
-      </Text>
-      <Flex
-        direction="column"
-        wrap="nowrap"
-        justify="center"
-        maxHeight="200px"
-        style={{ overflow: "scroll" }}
-      >
-        {/* <Text as="h3" size="4" weight="bold" color="indigo">Riddle Item List</Text> */}
-        <Table.Root>
-          {savedItems.length === 0 ? (
-            <Table.Header>
-              <Table.Row>
-                <Table.Cell colSpan={4}>added items appear here</Table.Cell>
-              </Table.Row>
-            </Table.Header>
-          ) : (
-            <>
-              <Table.Header>
-                <Table.Row>
-                  <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>Item</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>Riddle</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>
-                    Total : {savedItems.length}
-                  </Table.ColumnHeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {savedItems.map((item, idx) => {
-                  return (
-                    <Table.Row key={idx}>
-                      <Table.Cell>{idx + 1}</Table.Cell>
-                      <Table.Cell
-                        style={{
-                          maxWidth: "30vw",
-                          overflow: "hidden",
-                          whiteSpace: "nowrap",
-                          textOverflow: "ellipsis",
-                        }}>{item.item}</Table.Cell>
-                      <Table.Cell
-                        style={{
-                          maxWidth: "40vw",
-                          overflow: "hidden",
-                          whiteSpace: "nowrap",
-                          textOverflow: "ellipsis",
-                        }}
-                      >
-                        {item.riddle}
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Button
-                          variant="soft"
-                          onClick={(e) => deleteItemPair(idx)}
-                        >
-                          Delete
-                        </Button>
-                      </Table.Cell>
-                    </Table.Row>
-                  );
-                })}
-              </Table.Body>
-            </>
-          )}
-        </Table.Root>
-      </Flex>
-      <TextField.Root
-        type="text"
-        placeholder="Hunt Name"
-        value={huntName}
-        onChange={(e) => setHuntName(e.target.value)}
-        style={{ width: "300px" }}
-      >
-        <TextField.Slot />
-      </TextField.Root>
-      <TextArea
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        style={{ width: "300px", height: "50px" }}
-      />
-
-      <Flex
-        direction="column"
-        gap="10px"
-        style={{ width: "100%", maxWidth: "500px", padding: "10px" }}
-      >
-        <Flex
-          direction="row"
-          gap="10px"
-          align="center"
-          style={{ justifyContent: "space-between" }}
-        >
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <Button variant="surface" style={{ width: "48%" }}>
-                {selection?.category || "Select Category"}
-              </Button>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content>
-              {categories.map((category, idx) => (
-                <DropdownMenu.Item
-                  key={idx}
-                  onSelect={() => handleCategoryChange(category)}
-                >
-                  {category}
-                </DropdownMenu.Item>
-              ))}
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <Button variant="surface" style={{ width: "48%" }}>
-                {selection.item || "Select Item"}
-              </Button>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content>
-              {filteredItems.map((item, idx) => (
-                <DropdownMenu.Item
-                  key={idx}
-                  onSelect={() => handleItemChange(item.name, item.id)}
-                >
-                  {item.name}
-                </DropdownMenu.Item>
-              ))}
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
-        </Flex>
-        <input
-          type="text"
-          placeholder="Riddle"
-          value={selection.riddle}
-          onChange={(e) => handleRiddleChange(e.target.value)}
-          style={{
-            width: "100%",
-            padding: "10px",
-            borderRadius: "4px",
-            border: "1px solid #ccc",
-          }}
+      {!isTempCreated ? (
+        <Text as="h1" size="6" weight="bold" color="indigo" variant="soft">
+          Create a Scavenger Hunt
+        </Text>
+      ) : (
+        <>
+          <Text as="h1" size="6" weight="bold" color="indigo" variant="soft">
+            Add Riddle Items
+          </Text>
+          <RiddleItemList
+            savedItems={savedItems}
+            deleteItemPair={deleteItemPair}
+          />
+        </>
+      )}
+      {!isTempCreated ? (
+        <CreateHuntInputs
+          huntName={huntName}
+          setHuntName={setHuntName}
+          description={description}
+          setDescription={setDescription}
+          location={location}
+          setLocation={setLocation}
         />
-      </Flex>
-
-      <Button
-        onClick={addCategoryItemPair}
-        variant="soft"
-        style={{
-          width: "100%",
-          maxWidth: "500px",
-          cursor: "default",
-          transition: "none",
-        }}
-      >
-        Add Category & Item
-      </Button>
-      <Button
-        onClick={saveHunt}
-        variant="surface"
-        style={{
-          width: "100%",
-          maxWidth: "500px",
-          transition: "transform 0.2s, background-color 0.2s",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-        }}
-        onMouseOver={(e) => {
-          e.currentTarget.style.transform = "scale(1.05)";
-          e.currentTarget.style.backgroundColor = "#4B0082";
-        }}
-        onMouseOut={(e) => {
-          e.currentTarget.style.transform = "scale(1)";
-          e.currentTarget.style.backgroundColor = "";
-        }}
-      >
-        Create Scavenger Hunt
-      </Button>
+      ) : (
+        <RiddleItemSelection
+          selection={selection}
+          categories={categories}
+          filteredItems={filteredItems}
+          handleCategoryChange={handleCategoryChange}
+          handleItemChange={handleItemChange}
+          handleRiddleChange={handleRiddleChange}
+          addCategoryItemPair={addCategoryItemPair}
+        />
+      )}
+      <SubmitHuntBtn createHunt={createHunt} saveAndExit={saveAndExit} isTempCreated={isTempCreated} />
     </Flex>
   );
 };
