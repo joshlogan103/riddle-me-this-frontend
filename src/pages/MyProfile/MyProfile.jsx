e260b8c65ad59ee5be820d229ca791b97cb6e8e2
Josh Logan <joshlogan103@gmail.com>
2024-05-24 00:26:24 -0400

    add firebase functionality for profile images

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -1,36 +1,53 @@
 import { Avatar, Flex, Separator, Tabs, Text } from "@radix-ui/themes";
-import React, { useEffect, useRef, useState } from "react";
-import { getProfile } from "../../services/serviceRoutes/userServices";
+import { useEffect, useRef, useState } from "react";
+import { getProfile, updateProfile } from "../../services/serviceRoutes/userServices";
 import ProfileInstancesList from "../../components/ProfileInstancesList/ProfileInstancesList";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 import Loading from "../../components/Loading/Loading";
+import uploadProfileImage from "../../utils/firebaseUpload";
 
 const MyProfile = () => {
-  const [ pastHunts, setPastHunts ] = useState(null)
-  const [ upcomingHunts, setUpcomingHunts ] = useState(null)
-  const [ userData, setUserData ] = useState(null)
-  const [ loading, setLoading ] = useState(true)
+  const [pastHunts, setPastHunts] = useState(null);
+  const [upcomingHunts, setUpcomingHunts] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const fileInputRef = useRef(null)
+  const fileInputRef = useRef(null);
+
   const handleIconClick = () => {
-    fileInputRef.current.click()
-  }
-  const handleFileChange = (e) => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = async (e) => {
     const selectedFile = e.target.files[0];
-    console.log("upload this file", selectedFile)
-    // TODO: Firebase service that uploads userFile
-  }
+    if (!selectedFile) {
+      console.error("No file selected");
+      return;
+    }
+
+    console.log("upload this file", selectedFile);
+    try {
+      const downloadURL = await uploadProfileImage(selectedFile);
+      setUserData((prevUserData) => ({
+        ...prevUserData,
+        profile_pic: downloadURL,
+      }));
+      await updateProfile({ profile_pic: downloadURL });
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
 
   const filterParticipations = (participations) => {
-    const upcoming = participations.filter(item => {
-      return new Date(item.hunt_instance.end_time) > new Date()
-    })
-    const past = participations.filter(item => {
-      return new Date(item.hunt_instance.end_time) < new Date()
-    })
-    setPastHunts(past)
-    setUpcomingHunts(upcoming)
-  }
+    const upcoming = participations.filter((item) => {
+      return new Date(item.hunt_instance.end_time) > new Date();
+    });
+    const past = participations.filter((item) => {
+      return new Date(item.hunt_instance.end_time) < new Date();
+    });
+    setPastHunts(past);
+    setUpcomingHunts(upcoming);
+  };
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -38,36 +55,38 @@ const MyProfile = () => {
         const response = await getProfile();
         if (response.status === 200) {
           console.log(response);
-          // setParticipations(response.data.participations)
-          setUserData(response.data.profile[0])
-          setLoading(false)
-          filterParticipations(response.data.participations)
+          setUserData(response.data.profile[0]);
+          setLoading(false);
+          filterParticipations(response.data.participations);
         }
       } catch (error) {
         console.error(error);
-        setLoading(false)
+        setLoading(false);
       }
     };
     fetchUserData();
   }, []);
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
     <Flex direction="column" className="" align="center">
-      <div style={{position: "relative"}}>
+      <div style={{ position: "relative" }}>
         <Avatar src={userData?.profile_pic} fallback={userData?.user.username.charAt() || "U"} size="8" radius="full" m="6" />
-        <Pencil2Icon onClick={handleIconClick} style={{
-          position: "absolute",
-          bottom: 40,
-          right: 40,
-          height: "20px",
-          width: "20px",
-          padding: "10px"
-        }}/>
-        <input type="file" ref={fileInputRef} onChange={handleFileChange} style={{display: "none"}}/>
+        <Pencil2Icon
+          onClick={handleIconClick}
+          style={{
+            position: "absolute",
+            bottom: 40,
+            right: 40,
+            height: "20px",
+            width: "20px",
+            padding: "10px",
+          }}
+        />
+        <input type="file" ref={fileInputRef} onChange={handleFileChange} style={{ display: "none" }} />
       </div>
       <Text size="6">{userData?.user.username || "Username"}</Text>
       <Separator orientation="horizontal" m="7" style={{ width: "80%" }} />
@@ -79,10 +98,10 @@ const MyProfile = () => {
           </Tabs.List>
         </Flex>
         <Tabs.Content value="upcoming">
-          <ProfileInstancesList participations={upcomingHunts}/>
+          <ProfileInstancesList participations={upcomingHunts} />
         </Tabs.Content>
         <Tabs.Content value="previous">
-          <ProfileInstancesList participations={pastHunts}/>
+          <ProfileInstancesList participations={pastHunts} />
         </Tabs.Content>
       </Tabs.Root>
     </Flex>
