12502ac6cd9b3d3abd0e7782d121d25dc14ed6a6
dfergusbrown <dfergusbrown@gmail.com>
2024-05-23 23:12:11 -0400

    sets timer dependent on target hunt date/time

--- src/pages/ActiveHuntPage/ActiveHuntPage.jsx
+++ src/pages/ActiveHuntPage/ActiveHuntPage.jsx
@@ -1,10 +1,42 @@
 import { useState, useEffect } from "react";
 import RiddlesLayout from "../../components/RiddlesLayout/RiddlesLayout";
 import "./activeHuntPage.css";
+import { getHuntInstanceById } from "../../services/serviceRoutes/huntInstanceServices";
+import { useParams } from "react-router";
 
 const PreGameView = () => {
-  const [timeLeft, setTimeLeft] = useState(5); // 5 seconds
+  const [timeLeft, setTimeLeft] = useState(0); // 5 seconds
   const [showNewContent, setShowNewContent] = useState(false);
+  const { huntTemplateId, huntInstanceId } = useParams();
+
+  const calculateTimeLeft = (startTime) => {
+    const targetDate = new Date(startTime);
+    const currentDate = new Date();
+    console.log(targetDate, currentDate);
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+    const milliseconds = timeDifference;
+    console.log(Math.abs(milliseconds));
+
+    setTimeLeft(Math.abs(milliseconds));
+  };
+
+  useEffect(() => {
+    const fetchHuntInstance = async () => {
+      try {
+        const response = await getHuntInstanceById(
+          huntTemplateId,
+          huntInstanceId
+        );
+        // console.log(response.data)
+        if (response.status === 200) {
+          calculateTimeLeft(response.data.hunt_instance.start_time);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchHuntInstance();
+  }, []);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -19,23 +51,30 @@ const PreGameView = () => {
     }, 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [timeLeft]);
 
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+  const formatTime = (timeLeft) => {
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    const remainingSeconds = timeLeft % 60;
+    return `${days} days ${hours} hours ${minutes}:${remainingSeconds
+      .toString()
+      .padStart(2, "0")}`;
   };
 
   return (
     <div className="pregame-container">
       {!showNewContent ? (
         <>
-          <div className="header">
-          </div>
+          <div className="header"></div>
           <div className="content">
-            <p className="timer">{formatTime(timeLeft)} until the hunt begins</p>
-            </div>
+            <p className="timer">{formatTime(timeLeft)}</p>
+            <p> until the hunt begins</p>
+          </div>
         </>
       ) : (
         <RiddlesLayout />
