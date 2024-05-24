c15c38068b8c25c908106663ff24df0d273de06f
vitrineofcode <tilers_unable_0n@icloud.com>
2024-05-23 20:25:30 -0400

    fixed webcam distortion

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -14,8 +14,6 @@ const Camera = (props) => {
   const [imageSrc, setImageSrc] = useState(null);
   const [responseMessage, setResponseMessage] = useState(null);
   const [videoConstraints, setVideoConstraints] = useState({
-    width: 1280,
-    height: 720,
     facingMode: "environment"  // Attempt to use the rear camera on devices
   });
 
